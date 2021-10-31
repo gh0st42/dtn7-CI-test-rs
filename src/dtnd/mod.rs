0e5fa71037c574db2b708aa4957864cc9f23832b
Lars Baumgaertner <1264131+gh0st42@users.noreply.github.com>
2021-10-31 23:23:10 +0100

    test

--- src/dtnd/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod cron;
-pub mod daemon;
-pub mod httpd;
-pub mod janitor;
-pub mod ws;
--- src/ipnd/mod.rs
+++
@@ -1,4 +0,0 @@
-#![forbid(unsafe_code)]
-pub mod beacon;
-pub mod neighbour_discovery;
-pub mod services;
